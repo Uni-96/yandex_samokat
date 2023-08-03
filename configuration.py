71c44c55abf80ffeb13d6e075cd65397967bde2d
«Uni-96» <aleksiiants.sergei@gmail.com>
2023-08-03 21:24:16 +0500

    Оптимизация кода при переводе типа в str

--- configuration.py
+++ configuration.py
@@ -1,3 +1,3 @@
 URL_SERVICE = "https://7cd333ce-8e3e-4a17-a15f-3639a3283f47.serverhub.praktikum-services.ru"
 CREATE_ORDER_PATH = "/api/v1/orders"
-RECEIVE_ORDER_BY_NUMBER_PATH = "/api/v1/orders/track"
+RECEIVE_ORDER_BY_NUMBER_PATH = "/api/v1/orders/track?t="
